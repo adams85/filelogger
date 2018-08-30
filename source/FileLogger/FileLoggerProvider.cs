f273c5a7550dccf2e0fccdfcaa7694de051a9493
adams85 <adamosimoni@gmail.com>
2018-08-30 11:20:22 +0200

    settings change handling revised

--- source/FileLogger/FileLoggerProvider.cs
+++ source/FileLogger/FileLoggerProvider.cs
@@ -64,7 +64,7 @@ namespace Karambolo.Extensions.Logging.File
             _settingsChangeToken?.Dispose();
 
             // blocking in Dispose() seems to be a design flaw, however ConsoleLoggerProcess.Dispose() implemented this way as well
-            ResetProcessor(null);            
+            ResetProcessor(null);
             Processor.Dispose();
         }
 
@@ -99,9 +99,10 @@ namespace Karambolo.Extensions.Logging.File
 
                 foreach (var logger in _loggers.Values)
                     logger.Update(GetFallbackFileName(logger.CategoryName), Settings);
-            }
 
-            ResetProcessor(Settings);
+                // we must try to wait for the current queues to complete to avoid concurrent file I/O
+                ResetProcessor(Settings);
+            }
 
             return true;
         }
