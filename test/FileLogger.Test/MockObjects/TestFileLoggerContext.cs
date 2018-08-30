eecc5b299e8f9e2b1dab88f5563b69aebdc66f02
adams85 <adamosimoni@gmail.com>
2018-08-30 11:19:00 +0200

    file I/O responsibilities removed from IFileLoggerContext and encapsulated in IFileAppender

--- test/FileLogger.Test/MockObjects/TestFileLoggerContext.cs
+++ test/FileLogger.Test/MockObjects/TestFileLoggerContext.cs
@@ -9,53 +9,17 @@ namespace Karambolo.Extensions.Logging.File.Test.MockObjects
 {
     class TestFileLoggerContext : FileLoggerContext
     {
-        public TestFileLoggerContext() : this(CancellationToken.None) { }
+        public TestFileLoggerContext(CancellationToken completeToken = default)
+            : this(new MemoryFileAppender(), completeToken) { }
 
-        public TestFileLoggerContext(IFileProvider fileProvider) : this(fileProvider, CancellationToken.None) { }
+        public TestFileLoggerContext(PhysicalFileProvider fileProvider, CancellationToken completeToken = default)
+            : this(new PhysicalFileAppender(fileProvider), completeToken) { }
 
-        public TestFileLoggerContext(CancellationToken completeToken) : this(new MemoryFileProvider(), completeToken) { }
-
-        public TestFileLoggerContext(IFileProvider fileProvider, CancellationToken completeToken)
-            : base(fileProvider, "fallback.log", completeToken) { }
+        public TestFileLoggerContext(IFileAppender fileAppender, CancellationToken completeToken = default)
+            : base(fileAppender, "fallback.log", completeToken) { }
 
         DateTimeOffset _timestamp;
         public override DateTimeOffset GetTimestamp() => _timestamp;
         public void SetTimestamp(DateTimeOffset value) => _timestamp = value;
-
-        public override Task AppendAllTextAsync(IFileInfo fileInfo, string text, Encoding encoding)
-        {
-            if (FileProvider is MemoryFileProvider memoryFileProvider)
-            {
-                var memoryFileInfo = (MemoryFileInfo)fileInfo;
-
-                if (!memoryFileInfo.Exists)
-                    memoryFileProvider.CreateFile(fileInfo.PhysicalPath, null, encoding);
-                else if (memoryFileInfo.IsDirectory)
-                    throw new InvalidOperationException("A directory with the same path already exists.");
-
-                memoryFileProvider.WriteContent(fileInfo.PhysicalPath, text, append: true);
-
-                return Task.FromResult<object>(null);
-            }
-            else
-                return base.AppendAllTextAsync(fileInfo, text, encoding);
-        }
-
-        public override Task<bool> EnsureDirAsync(IFileInfo fileInfo)
-        {
-            if (FileProvider is MemoryFileProvider memoryFileProvider)
-            {
-                var memoryFileInfo = (MemoryFileInfo)fileInfo;
-
-                var dirPath = memoryFileProvider.GetFileInfo(Path.GetDirectoryName(fileInfo.PhysicalPath));
-                if (dirPath.Exists)
-                    return Task.FromResult(false);
-
-                memoryFileProvider.CreateDir(dirPath.PhysicalPath);
-                return Task.FromResult(true);
-            }
-            else
-                return base.EnsureDirAsync(fileInfo);
-        }
     }
 }
