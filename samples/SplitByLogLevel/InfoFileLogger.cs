7fdf85b6d542ed7559d080cbbe59673235e0046b
Adam Simon <adamosimoni@gmail.com>
2019-04-08 18:45:14 +0200

    multiple providers sample

---
+++ samples/SplitByLogLevel/InfoFileLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Karambolo.Extensions.Logging.File;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace SplitByLogLevel
+{
+    [ProviderAlias("InfoFile")] // use this alias in appsettings.json to configure this provider
+    class InfoFileLoggerProvider : FileLoggerProvider
+    {
+        public InfoFileLoggerProvider(IFileLoggerContext context, IOptionsMonitor<FileLoggerOptions> options, string optionsName) : base(context, options, optionsName) { }
+
+        protected override FileLogger CreateLoggerCore(string categoryName)
+        {
+            // we instantiate our derived file logger which is modified to log only messages with log level information or below
+            return new InfoFileLogger(categoryName, GetFallbackFileName(categoryName), Processor, Settings, GetScopeProvider(), Context.GetTimestamp);
+        }
+    }
+
+    class InfoFileLogger : FileLogger
+    {
+        public InfoFileLogger(string categoryName, string fallbackFileName, IFileLoggerProcessor processor, IFileLoggerSettingsBase settings, IExternalScopeProvider scopeProvider = null, Func<DateTimeOffset> timestampGetter = null)
+            : base(categoryName, fallbackFileName, processor, settings, scopeProvider, timestampGetter) { }
+
+        protected override bool IsEnabled(UpdatableState state, LogLevel logLevel)
+        {
+            return
+                logLevel <= LogLevel.Information &&  // don't allow messages more severe than information to pass through
+                base.IsEnabled(state, logLevel);
+        }
+    }
+}
---
+++ samples/SplitByLogLevel/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace SplitByLogLevel
+{
+    // This sample demonstrates how to use multiple file logger providers with different settings.
+    // We set up two provider instances to send messages to different files based on their log levels.
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var services = new ServiceCollection();
+
+            services.AddLogging(builder =>
+            {
+                builder.AddConfiguration(configuration.GetSection("Logging"));
+
+                // the "standard" provider which logs all messages with severity warning or above to 'warn+err.log' (see appsettings.json for configuration settings)
+                builder.AddFile(o => o.RootPath = AppContext.BaseDirectory);
+
+                // a custom one which only logs messages with level information or below to 'info.log'
+                builder.AddFile<InfoFileLoggerProvider>(configure: o => o.RootPath = AppContext.BaseDirectory);
+            });
+
+            // create logger factory
+            using (ServiceProvider sp = services.BuildServiceProvider())
+            {
+                var logger = sp.GetRequiredService<ILoggerFactory>().CreateLogger<Program>();
+
+                logger.LogTrace("This is a trace message. Should be discarded.");
+                logger.LogDebug("This is a debug message. Should be discarded.");
+                logger.LogInformation("This is an info message. Should go into 'info.log' only.");
+                logger.LogWarning("This is a warning message. Should go into 'warn+err.log' only.");
+                logger.LogError("This is an error message. Should go into 'warn+err.log' only.");
+                logger.LogCritical("This is a critical message. Should go into 'warn+err.log' only.");
+            }
+        }
+    }
+ }
