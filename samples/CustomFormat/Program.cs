5263fa0424f1fed0a1fddc7b0f7454fb59e27c53
Adam Simon <adamosimoni@gmail.com>
2021-01-30 14:42:32 +0100

    custom output format sample

---
+++ samples/CustomFormat/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace CustomFormat
+{
+    // This sample demonstrates how to customize the output of the logger by
+    // subclassing FileLogEntryTextBuilder and overriding its virtual methods.
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var services = new ServiceCollection();
+
+            services.AddLogging(builder =>
+            {
+                builder.AddConfiguration(configuration.GetSection("Logging"));
+
+                builder.AddFile(configure: o =>
+                {
+                    o.RootPath = AppContext.BaseDirectory;
+
+                    // This is how the custom text builder is configured by code,
+                    // but in this case we do that in appsettings.json.
+                    //o.TextBuilder = SingleLineLogEntryTextBuilder.Default;
+                });
+            });
+
+            await using (ServiceProvider sp = services.BuildServiceProvider())
+            {
+                // create logger
+                var logger = sp.GetRequiredService<ILogger<Program>>();
+
+                logger.LogInformation("A non-scoped message.");
+                using (logger.BeginScope("A scope"))
+                {
+                    logger.LogInformation("A scoped message.");
+
+                    using (logger.BeginScope("Another scope"))
+                    {
+                        try { throw new ApplicationException("Some error."); }
+                        catch (Exception ex) { logger.LogInformation(ex, $"Another scoped multi-line message{Environment.NewLine}with exception."); }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ samples/CustomFormat/SingleLineLogEntryTextBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using Karambolo.Extensions.Logging.File;
+using Microsoft.Extensions.Logging;
+
+namespace CustomFormat
+{
+    internal class SingleLineLogEntryTextBuilder : FileLogEntryTextBuilder
+    {
+        public static readonly SingleLineLogEntryTextBuilder Default = new SingleLineLogEntryTextBuilder();
+
+        protected override void AppendTimestamp(StringBuilder sb, DateTimeOffset timestamp)
+        {
+            sb.Append(" @ ").Append(timestamp.ToLocalTime().ToString("o", CultureInfo.InvariantCulture));
+        }
+
+        protected override void AppendLogScopeInfo(StringBuilder sb, IExternalScopeProvider scopeProvider)
+        {
+            scopeProvider.ForEachScope((scope, builder) =>
+            {
+                builder.Append(' ');
+
+                AppendLogScope(builder, scope);
+            }, sb);
+        }
+
+        protected override void AppendMessage(StringBuilder sb, string message)
+        {
+            sb.Append(" => ");
+
+            var length = sb.Length;
+            sb.AppendLine(message);
+            sb.Replace(Environment.NewLine, " ", length, message.Length);
+        }
+    }
+}
