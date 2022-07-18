5be852456ab7f672a1806fca1b73c6fda2550aa8
Adam Simon <adamosimoni@gmail.com>
2022-07-18 17:13:13 +0200

    pushtonuget script updated

--- release/pushtonuget.cmd
+++ release/pushtonuget.cmd
@@ -5,4 +5,5 @@ SET APIKEY=%2
 SET SOURCE=https://api.nuget.org/v3/index.json
 
 dotnet nuget push Karambolo.Extensions.Logging.File.%PKGVER%.nupkg -k %APIKEY% -s %SOURCE%
+dotnet nuget push Karambolo.Extensions.Logging.File.Json.%PKGVER%.nupkg -k %APIKEY% -s %SOURCE%
 IF %ERRORLEVEL% NEQ 0 goto:eof
