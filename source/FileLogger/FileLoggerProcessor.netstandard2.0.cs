9253665612c6a406cc4f512716f6cd0f4750211d
Sander Saares <sander@saares.eu>
2023-02-25 22:21:58 +0200

    Use ArrayPool for encoding buffers in netstandard2.1. (#25)
    This removes byte[] allocations for writing each line, greatly reducing the allocation rate of the library.

--- source/FileLogger/FileLoggerProcessor.netstandard2.0.cs
+++ source/FileLogger/FileLoggerProcessor.netstandard2.0.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Karambolo.Extensions.Logging.File
@@ -7,6 +8,12 @@ namespace Karambolo.Extensions.Logging.File
     {
         protected internal partial class LogFileInfo
         {
+            internal Task WriteTextAsync(string text, Encoding encoding, CancellationToken cancellationToken)
+            {
+                var bytes = encoding.GetBytes(text);
+                return _appendStream.WriteAsync(bytes, 0, bytes.Length, cancellationToken);
+            }
+
             internal Task WriteBytesAsync(byte[] bytes, CancellationToken cancellationToken)
             {
                 return _appendStream.WriteAsync(bytes, 0, bytes.Length, cancellationToken);
--- source/FileLogger/FileLoggerProcessor.netstandard2.1.cs
+++ source/FileLogger/FileLoggerProcessor.netstandard2.1.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading;
+﻿using System;
+using System.Buffers;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Karambolo.Extensions.Logging.File
@@ -7,9 +10,24 @@ namespace Karambolo.Extensions.Logging.File
     {
         protected internal partial class LogFileInfo
         {
-            internal ValueTask WriteBytesAsync(byte[] bytes, CancellationToken cancellationToken)
+            internal async ValueTask WriteTextAsync(string text, Encoding encoding, CancellationToken cancellationToken)
             {
-                return _appendStream.WriteAsync(bytes, cancellationToken);
+                var buffer = ArrayPool<byte>.Shared.Rent(encoding.GetMaxByteCount(text.Length));
+
+                try
+                {
+                    var byteCount = encoding.GetBytes(text, buffer);
+                    await _appendStream.WriteAsync(buffer.AsMemory(0, byteCount), cancellationToken).ConfigureAwait(false);
+                }
+                finally
+                {
+                    ArrayPool<byte>.Shared.Return(buffer);
+                }
+            }
+
+            internal Task WriteBytesAsync(byte[] bytes, CancellationToken cancellationToken)
+            {
+                return _appendStream.WriteAsync(bytes, 0, bytes.Length, cancellationToken);
             }
         }
     }
