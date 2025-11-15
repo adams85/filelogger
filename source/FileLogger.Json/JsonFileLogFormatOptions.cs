de3b2de53e2e95bfd1d0b57f0235bf18760927f4
Adam Simon <adamosimoni@gmail.com>
2025-11-15 11:47:06 +0100

    Add helper method for creating options for JSON lines (JsonFileLogFormatOptions.ForJsonLines)

--- source/FileLogger.Json/JsonFileLogFormatOptions.cs
+++ source/FileLogger.Json/JsonFileLogFormatOptions.cs
@@ -1,9 +1,19 @@
-﻿using System.Text.Json;
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
 
 namespace Karambolo.Extensions.Logging.File.Json
 {
     public class JsonFileLogFormatOptions
     {
+        public static JsonFileLogFormatOptions ForJsonLines(JavaScriptEncoder encoder = null)
+        {
+            return new JsonFileLogFormatOptions
+            {
+                EntrySeparator = "",
+                JsonWriterOptions = new JsonWriterOptions { Indented = false, Encoder = encoder }
+            };
+        }
+
         public JsonWriterOptions? JsonWriterOptions { get; set; }
         public string EntrySeparator { get; set; }
     }
